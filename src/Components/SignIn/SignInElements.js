4a955ef3bc3eaf0df33188aa03e097e655eb2439
shreyaa <shetshreyaa25@gmail.com>
2022-02-05 23:09:15 +0530

    style:signin pg

---
+++ src/Components/SignIn/SignInElements.js
@@ -0,0 +1,18 @@
+import styled from "@emotion/styled";
+export const SignInContainer = styled.div`
+  display:flex;
+  height:100vh;
+`;
+export const ImgWrapper = styled.div`
+  width:50%;
+  height:100%;
+  display:flex;
+  justify-content:center;
+  align-items:center;
+  background-color: #2667FF;
+  flex-direction:column;
+  color:#fff;
+  @media screen and (max-width: 800px) {
+    display: none;
+  }
+`;
--- src/Components/SignIn/index.jsx
+++ src/Components/SignIn/index.jsx
@@ -1,28 +1,96 @@
 import { Box } from "@mui/material";
 import React from "react";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
 
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+
+
+import { ImgWrapper, SignInContainer } from "./SignInElements";
 const SignIn = () => {
   return (
     <>
-      <Box id="signin">
-        <div style={{ display: "flex", height: "100vh" }}>
-          <div
-            style={{
-              width: "50%",
-              height: "100%",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              background: "#2667FF",
-              flexDirection: "column",
-              color: "#fff",
-            }}
-          >
-            <img src="static/img/login.png" style={{ width: "70%" }} alt="" />
+      <Box id='signin'>
+        <SignInContainer>
+          <ImgWrapper>
+            <img src='static/img/login.png' style={{ width: "70%" }} alt='' />
             <h1>Welcome Back</h1>
-          </div>
-          <div></div>
-        </div>
+          </ImgWrapper>
+
+          <Grid item md={6} sx={{ justifyContent: "center", margin: "auto" }}>
+            <Container component='main' maxWidth='xs'>
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                  marginBottom: 2,
+                }}
+              >
+                <AccountCircleOutlinedIcon
+                  style={{ fontSize: "4rem", color: "#2667FF" }}
+                />
+                <Typography component='h2' variant='h5'>
+                  Sign to your account
+                </Typography>
+                <Box component='form' noValidate sx={{ mt: 1, padding: 1 }}>
+                  <TextField
+                    margin='normal'
+                    required
+                    fullWidth
+                    id='email'
+                    label='Email Address'
+                    name='email'
+                    autoComplete='email'
+                    autoFocus
+                  />
+                  <TextField
+                    margin='normal'
+                    required
+                    fullWidth
+                    name='password'
+                    label='Password'
+                    type={"password"}
+                    id='password'
+                    autoComplete='current-password'
+                  />
+
+                  <Button
+                    type='submit'
+                    fullWidth
+                    variant='contained'
+                    sx={{ mt: 3, mb: 2 }}
+                    disableElevation
+                  >
+                    Sign In
+                  </Button>
+
+                  <Grid container sx={{ paddingTop: 2 }}>
+                    <Grid item xs>
+                    
+                        <Link href='#' variant='body2'>
+                          {"Forgot Password"}
+                        </Link>
+                 
+                    </Grid>
+                    <Grid item>
+                       
+                        <Link href='#' variant='body2'>
+                          {"Don't have an account? Sign Up"}
+                        </Link>
+                 
+                    </Grid>
+                  </Grid>
+                </Box>
+              </Box>
+            </Container>
+          </Grid>
+          {/* </Box> */}
+        </SignInContainer>
       </Box>
     </>
   );
