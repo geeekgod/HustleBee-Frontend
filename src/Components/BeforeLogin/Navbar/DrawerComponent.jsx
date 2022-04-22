3aedda6bca1287d17a1ebe7bc2467cae625c5e2f
shreyaa <shetshreyaa25@gmail.com>
2022-04-22 12:49:49 +0530

    stle:landing page buttons

--- src/Components/BeforeLogin/Navbar/DrawerComponent.jsx
+++ src/Components/BeforeLogin/Navbar/DrawerComponent.jsx
@@ -1,17 +1,23 @@
 import React from "react";
 import {
-  // Box,
+  Box,
   Drawer,
+  Link,
   List,
   ListItem,
   ListItemIcon,
   ListItemText,
+
 } from "@mui/material";
+
+import CustomBtn from "../Main/CustomBtn";
+// import CtaButton from "../Main/CtaButtons";
+
 
 const DrawerComponent = ({ openDrawer, setOpenDrawer }) => {
   return (
     <Drawer
-      anchor="left"
+    anchor="right"
       open={openDrawer}
       onClose={() => {
         setOpenDrawer(false);
@@ -19,20 +25,50 @@ const DrawerComponent = ({ openDrawer, setOpenDrawer }) => {
     >
       <List>
         <ListItem divider button onClick={() => setOpenDrawer(false)}>
-          <ListItemIcon sx={{ px: 5 }}>
+          <ListItemIcon sx={{ px: 10 }}>
             <ListItemText>Home</ListItemText>
           </ListItemIcon>
         </ListItem>
         <ListItem divider button onClick={() => setOpenDrawer(false)}>
-          <ListItemIcon sx={{ px: 5 }}>
+          <ListItemIcon sx={{ px: 10 }}>
             <ListItemText>Browse Jobs</ListItemText>
           </ListItemIcon>
         </ListItem>
         <ListItem divider button onClick={() => setOpenDrawer(false)}>
-          <ListItemIcon sx={{ px: 5 }}>
+          <ListItemIcon sx={{ px: 10 }}>
             <ListItemText>Contact</ListItemText>
           </ListItemIcon>
         </ListItem>
+        {/* buttons */}
+        <Box sx={{display:"flex",flexDirection:"column",justifyContent:"center"}}>
+          <Box sx={{marginTop: "20px" }}>
+          <Link to="/">
+                    <CustomBtn
+                      sx={{
+                       width:"80%",margin:"auto",
+                        display: "flex",alignItems:"center",justifyContent:"center"
+                      }}
+                    >
+                      Sign Up
+                    </CustomBtn>
+                  </Link>
+          </Box>
+                
+                    <Box sx={{marginTop: "20px" }}>
+                    <Link to="/signin">
+                    <CustomBtn
+                      sx={{
+                        width:"80%",margin:"auto",
+                        display: "flex",alignItems:"center",justifyContent:"center"
+                      }}
+                    >
+                      Log In
+                    </CustomBtn>
+                    
+                  </Link>
+                    </Box>
+               
+                </Box>
       </List>
     </Drawer>
   );
