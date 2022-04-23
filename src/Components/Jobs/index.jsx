814d0f086aa4323e9ce0224cb1ae80493f1a3c22
shreyaa <shetshreyaa25@gmail.com>
2022-04-23 13:05:33 +0530

    style:jobs ui

--- src/Components/Jobs/index.jsx
+++ src/Components/Jobs/index.jsx
@@ -1,1 +1,26 @@
+import { Container, Typography } from '@mui/material'
+import { Box } from '@mui/system'
+import React from 'react'
 
+const Jobs = () => {
+  return (
+    <>
+    <Box>
+        <Container>
+            <Box>
+            <Typography>
+                Find Jobs
+            </Typography>
+            </Box>
+            {/* job cards  */}
+            <Box>
+
+            </Box>
+
+        </Container>
+    </Box>
+    </>
+  )
+}
+
+export default Jobs
